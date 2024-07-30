c4c32d9e29d7c47515837a74162389e752e47c3e
santoshlokhande <lokhandesh@gmail.com>
2024-07-30 19:26:33 +0800

    Initial Commit

---
+++ FirstIOSApp/FirstIOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstIOSAppApp.swift
+//  FirstIOSApp
+//
+//  Created by santosh lokhande on 30/7/24.
+//
+
+import SwiftUI
+
+@main
+struct FirstIOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
